c579f2c8e93d6ff12a0ecaf9ffddf5d7af89517a
ciborg971 <yohann.belair@epita.fr>
2020-04-30 21:35:23 +0200

    Fix headless : remove space before cores

--- headless.sh
+++ headless.sh
@@ -36,7 +36,7 @@ qemu-system-x86_64 \
     -enable-kvm \
     -m $MEM \
     -machine q35,accel=kvm \
-    -smp $CPUS, cores=$CORES \
+    -smp $CPUS,cores=$CORES \
     -cpu Penryn,vendor=GenuineIntel,kvm=on,+sse3,+sse4.2,+aes,+xsave,+avx,+xsaveopt,+xsavec,+xgetbv1,+avx2,+bmi2,+smep,+bmi1,+fma,+movbe,+invtsc \
     -device isa-applesmc,osk="$OSK" \
     -smbios type=2 \
